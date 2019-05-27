4116eb905e52c56fe8c0a52c868472771cd4ae29
gio <giovag1995@gmail.com>
2019-05-27 11:59:00 -0400

    cambio de colores en la app quite el toolbar en la perdidaactivity y activity_perdida para que me dejara de dar error al poner el titulo de la app permitiendome la navegacion ancestral en la app

--- app/src/main/java/com/example/android/mytesis/MainActivity.java
+++ app/src/main/java/com/example/android/mytesis/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.android.mytesis;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/example/android/mytesis/PerdidaActivity.java
+++ app/src/main/java/com/example/android/mytesis/PerdidaActivity.java
@@ -2,6 +2,7 @@ package com.example.android.mytesis;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,8 +10,6 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
@@ -20,9 +19,7 @@ public class PerdidaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perdida);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create an instance of the tab layout from the view.
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
 // Set the text for each tab.
         tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_label5));
